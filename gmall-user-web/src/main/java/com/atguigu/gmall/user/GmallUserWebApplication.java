a78bc54454739a1ef1ebdb3ee44eb11c0b1de552
luojun <979592209@qq.com>
2019-08-16 10:25:25 +0800

    提交

---
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/GmallUserWebApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChinesePanda
+ */
+@SpringBootApplication
+public class GmallUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserWebApplication.class, args);
+    }
+
+}
