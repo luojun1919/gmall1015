7f5eb73109911522b7a709de72912656da5e8642
luojun <979592209@qq.com>
2019-08-19 21:46:57 +0800

    提交

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -1,11 +1,14 @@
 package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.PmsProductImage;
 import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.bean.PmsProductSaleAttr;
 import com.atguigu.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import util.PmsUploadUtil;
 
 import java.util.List;
 
@@ -46,12 +49,12 @@ public class SpuController {
     public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
         //将图片上传到分布式fastDFS文件服务器上
         //将图片的访问路径返回到前台
-        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+        return PmsUploadUtil.uploadImage(multipartFile);
     }
 
 
     /**
-     * @Description: 保存商品
+     * @Description: 保存商品spu
      * @param: [pmsProductInfo]
      * @return: java.lang.String
      * @Date: 2019/8/15 20:38
@@ -59,8 +62,34 @@ public class SpuController {
     @RequestMapping("saveSpuInfo")
     @ResponseBody
     public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
-
-        return "";
+        return spuService.saveSpuInfo(pmsProductInfo);
     }
+
+
+    /**
+     * @Description: sku
+     * @param: [spuId]
+     * @return: java.lang.String
+     * @Date: 2019/8/15 20:38
+     */
+    @RequestMapping("spuSaleAttrList")
+    @ResponseBody
+    public List<PmsProductSaleAttr> spuSaleAttrList(String spuId){
+        return spuService.spuSaleAttrList(spuId);
+    }
+
+
+    /**
+     * @Description: sku
+     * @param: [spuId]
+     * @return: java.lang.String
+     * @Date: 2019/8/15 20:38
+     */
+    @RequestMapping("spuImageList")
+    @ResponseBody
+    public List<PmsProductImage> spuImageList(String spuId){
+        return spuService.spuImageList(spuId);
+    }
+
 
 }
