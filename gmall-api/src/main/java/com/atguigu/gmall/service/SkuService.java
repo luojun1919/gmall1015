f349e3ddc19d68f8f6a60f797865119e1ae64bc8
luojun <979592209@qq.com>
2019-08-19 21:44:11 +0800

    提交

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/SkuService.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.PmsSkuInfo;
+
+/**
+ * @author ChinesePanda
+ * @ClassName: SkuService
+ * @Description: 数据服务接口
+ * @Auther: luoJun
+ * @Date: 2019/8/19 20:44
+ */
+public interface SkuService {
+    String saveSkuInfo(PmsSkuInfo pmsSkuInfo);
+}
