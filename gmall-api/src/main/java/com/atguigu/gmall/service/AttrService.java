5c4832ec9bf61ed7203c62cf6dda1bdcf0f68f4f
luojun <979592209@qq.com>
2019-08-19 21:45:57 +0800

    提交

--- gmall-api/src/main/java/com/atguigu/gmall/service/AttrService.java
+++ gmall-api/src/main/java/com/atguigu/gmall/service/AttrService.java
@@ -15,11 +15,35 @@ import java.util.List;
  */
 public interface AttrService {
 
+    /**
+     * @Description:
+     * @param: [catalog3Id]
+     * @return: java.util.List<com.atguigu.gmall.bean.PmsBaseAttrInfo>
+     * @Date: 2019/8/19 15:59
+     */
     List<PmsBaseAttrInfo> attrInfoList(String catalog3Id);
 
+    /**
+     * @Description:
+     * @param: [attrId]
+     * @return: java.util.List<com.atguigu.gmall.bean.PmsBaseAttrValue>
+     * @Date: 2019/8/19 15:59
+     */
     List<PmsBaseAttrValue> getAttrValueList(String attrId);
 
+    /**
+     * @Description:
+     * @param: [pmsBaseAttrInfo]
+     * @return: java.lang.String
+     * @Date: 2019/8/19 15:59
+     */
     String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo);
 
+    /**
+     * @Description:
+     * @param: []
+     * @return: java.util.List<com.atguigu.gmall.bean.PmsBaseSaleAttr>
+     * @Date: 2019/8/19 16:00
+     */
     List<PmsBaseSaleAttr> baseSaleAttrList();
 }
